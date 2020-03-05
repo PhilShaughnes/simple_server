555e5b15989c45b0525e35b6799cc63b3c2c7715
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2020-03-05 10:35:57 -0500

    Add error handling in resolver

--- lib/simple_server/schema/resolvers.ex
+++ lib/simple_server/schema/resolvers.ex
@@ -6,7 +6,10 @@ defmodule SimpleServer.Schema.Resolvers do
   }
 
   def get_item(%{id: item_id}, _info) do
-    {:ok, @items[item_id]}
+    case @items[item_id] do
+      nil -> {:error, "no item for id #{item_id} found"}
+      item -> {:ok, item}
+    end
   end
 
   def get_all_items(_args, _info) do
